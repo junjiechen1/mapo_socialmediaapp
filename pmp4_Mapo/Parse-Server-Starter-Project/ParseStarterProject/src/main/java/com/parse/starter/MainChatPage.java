5978b87419cfddcc3571a9d663bcb426bba83e8b
junjiechen1 <37597962+junjiechen1@users.noreply.github.com>
2019-02-21 13:32:51 -0500

    clean build resolve all my error by removing my google map

--- pmp4_Mapo/Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/MainChatPage.java
+++ pmp4_Mapo/Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/MainChatPage.java
@@ -10,15 +10,22 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainChatPage extends AppCompatActivity {
-
+/*
     ArrayList<String> users = new ArrayList<>();
     ArrayAdapter arrayAdapter;
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_chat_page);
+        /*
         ListView userlistview = (ListView)findViewById(R.id.userlist);
+        users.add("jun");
+        users.add("jake");
 
+        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,users);
+        userlistview.setAdapter(arrayAdapter);
+        */
     }
     public void maplocation(View view){
         Intent intent = new Intent(this,Mainmap.class);
