0abbf22d994ac064aba920e8460638c25f68d93c
junjiechen1 <37597962+junjiechen1@users.noreply.github.com>
2019-02-21 09:46:19 -0500

    added android.support.v7.widget.RecyclerView for friend list in chat page

--- pmp4_Mapo/Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/MainChatPage.java
+++ pmp4_Mapo/Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/MainChatPage.java
@@ -4,13 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainChatPage extends AppCompatActivity {
 
+    ArrayList<String> users = new ArrayList<>();
+    ArrayAdapter arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_chat_page);
+        ListView userlistview = (ListView)findViewById(R.id.userlist);
 
     }
     public void maplocation(View view){
