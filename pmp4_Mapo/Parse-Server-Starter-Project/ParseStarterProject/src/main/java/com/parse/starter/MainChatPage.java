aea36d4228ca41cda8221f3ea876864485a85dae
junjiechen1 <37597962+junjiechen1@users.noreply.github.com>
2019-02-24 21:41:48 -0500

    implement cardview to build.gradle

--- pmp4_Mapo/Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/MainChatPage.java
+++ pmp4_Mapo/Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/MainChatPage.java
@@ -3,29 +3,24 @@ package com.parse.starter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainChatPage extends AppCompatActivity {
-/*
-    ArrayList<String> users = new ArrayList<>();
-    ArrayAdapter arrayAdapter;
-    */
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_chat_page);
-        /*
-        ListView userlistview = (ListView)findViewById(R.id.userlist);
-        users.add("jun");
-        users.add("jake");
 
-        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,users);
-        userlistview.setAdapter(arrayAdapter);
-        */
+
     }
     public void maplocation(View view){
         Intent intent = new Intent(this,Mainmap.class);
